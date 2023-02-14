8db7c4b73ff56b0d230bb5bfab5b26e46d50bb55
mwoolf87 <mwoolfdev@gmail.com>
2023-02-14 16:47:51 -0500

    added prop-types to package.json to manage button props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Button from "./Button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>Click Me!!</Button>
+      </div>
+      <div>
+        <Button>Buy Now!</Button>
+      </div>
+      <div>
+        <Button>See Deal!</Button>
+      </div>
+      <div>
+        <Button>Checkout!</Button>
+      </div>
+      <div>
+        <Button>Flash Sale!!!!</Button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
