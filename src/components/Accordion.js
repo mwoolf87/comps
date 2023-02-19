fe0c7e106105a4d73cb546a5265cbf867133aa41
mwoolf87 <mwoolfdev@gmail.com>
2023-02-19 12:53:13 -0500

    completed accordion expand and contract logic and styled with TWCSS

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
-function Accordion() {
-  return <div>Accordion</div>;
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
+
+    return (
+      <div key={item.id}>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() =>
+            expandedIndex === index
+              ? setExpandedIndex(-1)
+              : setExpandedIndex(index)
+          }
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
+    );
+  });
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
