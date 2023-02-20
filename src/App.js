663106dcb71fdf635870fa579eb7981da50d512f
mwoolf87 <mwoolfdev@gmail.com>
2023-02-20 13:19:52 -0500

    created DropdownPage component for drop down state finished dropdown.js component to collapse when clicked outside of the dropdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import React from "react";
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 const App = () => {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = option => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" }
-  ];
-
-  return (
-    <Dropdown options={options} value={selection} onChange={handleSelect} />
-  );
+  return <div></div>;
 };
 
 export default App;
