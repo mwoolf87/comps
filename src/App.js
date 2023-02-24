0f0e904f9ffdda9292f20b16e66ddd51bc10494b
mwoolf87 <mwoolfdev@gmail.com>
2023-02-23 22:20:00 -0500

    created Link and Route components as well as navigation hook imported Link and Route components to App.js do display Accordion/Dropdown/Button pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import ButtonPage from "./pages/ButtonPage";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/accordion">Go to accordion</Link>
+      <Link to="/dropdown">Go to dropdown</Link>
+      <Link to="/button">Go to Button</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+        <Route path="/button">
+          <ButtonPage />
+        </Route>
+      </div>
+    </div>
+  );
 };
 
 export default App;
