a8edb1f7babc8a9330d1f179dc6e1f18b4bb7a82
mwoolf87 <mwoolfdev@gmail.com>
2023-02-23 23:28:40 -0500

    imported sidebare component to app component to replace link component in app function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Link from "./components/Link";
+import Sidebar from "./components/Sidebar";
 import Route from "./components/Route";
 import AccordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
@@ -6,18 +6,16 @@ import ButtonPage from "./pages/ButtonPage";
 
 const App = () => {
   return (
-    <div>
-      <Link to="/accordion">Go to accordion</Link>
-      <Link to="/dropdown">Go to dropdown</Link>
-      <Link to="/button">Go to Button</Link>
-      <div>
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
         <Route path="/accordion">
           <AccordionPage />
         </Route>
-        <Route path="/dropdown">
+        <Route path="/">
           <DropdownPage />
         </Route>
-        <Route path="/button">
+        <Route path="/buttons">
           <ButtonPage />
         </Route>
       </div>
