cfd49cb72af263743b330d7532054102fc35d814
mwoolf87 <mwoolfdev@gmail.com>
2023-02-28 13:43:28 -0500

    refactored table to dynamically create table columns and render specified data on lines 12-19 in TablePage

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,29 @@
 import React from "react";
 
-const Table = ({ data }) => {
+const Table = ({ data, config }) => {
+  const renderedHeaders = config.map(column => {
+    return <th key={column.label}>{column.label}</th>;
+  });
   const renderedRows = data.map(fruit => {
+    const renderedCells = config.map(column => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(fruit)}
+        </td>
+      );
+    });
     return (
-      <tr key={fruit.name}>
-        <td>{fruit.name}</td>
-        <td>{fruit.color}</td>
-        <td>{fruit.score}</td>
+      <tr className="border-b" key={fruit.name}>
+        {renderedCells}
       </tr>
     );
   });
 
   return (
-    <table>
-      <tr>
-        <th>Fruit</th>
-        <th>Color</th>
-        <th>Score</th>
-      </tr>
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
       <tbody>{renderedRows}</tbody>
     </table>
   );
