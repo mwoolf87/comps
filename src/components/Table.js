32ff69297a332f52cf09302d418c71a81d3fc1ca
mwoolf87 <mwoolfdev@gmail.com>
2023-02-28 13:02:09 -0500

    added in table data in TablePage, mapped over data prop to full table body info

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const Table = () => {
-  return <div>Table</div>;
+const Table = ({ data }) => {
+  const renderedRows = data.map(fruit => {
+    return (
+      <tr key={fruit.name}>
+        <td>{fruit.name}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <tr>
+        <th>Fruit</th>
+        <th>Color</th>
+        <th>Score</th>
+      </tr>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 };
 
 export default Table;
