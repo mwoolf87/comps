2fffb1a07c0b54942cf554ea47516450c4ad5bff
mwoolf87 <mwoolfdev@gmail.com>
2023-03-02 20:55:19 -0500

    created SortableTable component which takes in the Table component and adds logic to sort data by ascending or descending

---
+++ src/components/SortableTable.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Table from "./Table";
+import { useState } from "react";
+const SortableTable = props => {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const { config, data } = props;
+
+  const handleClick = label => {
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
+
+  const updatedConfig = config.map(column => {
+    if (!column.sortValue) {
+      return column;
+    }
+    return {
+      ...column,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.label} Is Sortable
+        </th>
+      )
+    };
+  });
+
+  // Only sort data if sortOrder && sortBy are not null
+  // Make a copy of the 'data' prop
+  // Find the correct sortValue function and use it for sorting
+
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find(column => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
+
+  return (
+    <div>
+      <Table {...props} data={sortedData} config={updatedConfig} />
+    </div>
+  );
+};
+
+export default SortableTable;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Table from "../components/Table";
+// import Table from '../components/Table';
+import SortableTable from "../components/SortableTable";
 
 const TablePage = () => {
   const data = [
@@ -10,12 +11,20 @@ const TablePage = () => {
   ];
 
   const config = [
-    { label: "Name", render: fruit => fruit.name },
+    {
+      label: "Name",
+      render: fruit => fruit.name,
+      sortValue: fruit => fruit.name
+    },
     {
       label: "Color",
       render: fruit => <div className={`p-3 m-2 ${fruit.color}`}></div>
     },
-    { label: "Score", render: fruit => fruit.score }
+    {
+      label: "Score",
+      render: fruit => fruit.score,
+      sortValue: fruit => fruit.score
+    }
   ];
 
   const keyFn = fruit => {
@@ -24,7 +33,7 @@ const TablePage = () => {
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 };
